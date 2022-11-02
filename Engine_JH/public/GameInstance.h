a1cfef262fb4e1a28db25bf22f63c92c22c8c839
MATTTTJ <hoon3022@naver.com>
2022-11-03 08:28:54 +0900

    빌드 오류 수정

--- Engine_JH/public/GameInstance.h
+++ Engine_JH/public/GameInstance.h
@@ -10,7 +10,7 @@ BEGIN(Engine)
 
 class ENGINE_DLL CGameInstance final : public CBase
 {
-	DECLARE_SINGLETON(CGameInstance);
+	DECLARE_SINGLETON(CGameInstance)
 private:
 	CGameInstance();
 	virtual ~CGameInstance() = default;
