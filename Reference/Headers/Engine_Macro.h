df95d737faa396f3b02f1c0b89663e8ab4c011a3
MATTTTJ <104554976+MATTTTJ@users.noreply.github.com>
2022-11-21 21:39:48 +0900

    매크로 추가

--- Reference/Headers/Engine_Macro.h
+++ Reference/Headers/Engine_Macro.h
@@ -1,5 +1,37 @@
 #pragma once
 
+#define NULL_CHECK( _ptr)	\
+	{if( _ptr == 0){__debugbreak();return;}}
+
+#define NULL_CHECK_RETURN( _ptr, _return)	\
+	{if( _ptr == 0){__debugbreak();return _return;}}
+
+#define NULL_CHECK_MSG( _ptr, _message )		\
+	{if( _ptr == 0){MessageBox(NULL, _message, L"System Message",MB_OK);__debugbreak();}}
+
+#define NULL_CHECK_RETURN_MSG( _ptr, _return, _message )	\
+	{if( _ptr == 0){MessageBox(NULL, _message, L"System Message",MB_OK);__debugbreak();return _return;}}
+
+
+#define FAILED_CHECK(_hr)	if( ((HRESULT)(_hr)) < 0 )	\
+	{ MessageBoxW(NULL, L"Failed", L"System Error",MB_OK); __debugbreak(); return E_FAIL;}
+
+#define FAILED_CHECK_RETURN(_hr, _return)	if( ((HRESULT)(_hr)) < 0 )		\
+	{ MessageBoxW(NULL, L"Failed", L"System Error",MB_OK); __debugbreak(); return _return;}
+
+#define FAILED_CHECK_MSG( _hr, _message)	if( ((HRESULT)(_hr)) < 0 )	\
+	{ MessageBoxW(NULL, _message, L"System Message",MB_OK); __debugbreak();return E_FAIL;}
+
+#define FAILED_CHECK_RETURN_MSG( _hr, _return, _message)	if( ((HRESULT)(_hr)) < 0 )	\
+	{ MessageBoxW(NULL, _message, L"System Message",MB_OK); __debugbreak();return _return;}
+
+
+
+
+
+
+
+
 #define GET_INSTANCE(CLASSNAME)	[](){											\
 	CLASSNAME*	pInstance = CLASSNAME::GetInstance();							\
 	if(nullptr == pInstance) {													\
